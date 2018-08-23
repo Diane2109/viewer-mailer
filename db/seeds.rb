6f182c20955787430785142d1711c386726fb238
Diane <d.bunod@gmail.com>
2018-08-23 23:17:32 +0200

    init

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Email.all.destroy_all
+
+Email.create(objet:"Objet_1 En GROS", body:"Contenu de l'email n°1.")
+
+Email.create(objet:"Objet_2 en gros", body:"Contenu de l'email n°2.")
+
+Email.create(objet:"Objet_3 tjrs en gros", body:"Contenu de l'email n°3.")
+
+Email.create(objet:"Objet_4 encore en GROS", body:"Contenu de l'email n°4.")
+
+Email.create(objet:"Objet_5 WESH EN GROS on a dit", body:"Contenu de l'email n°5.")
